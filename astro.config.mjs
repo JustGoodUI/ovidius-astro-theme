7dfe348f6c95769758da95b8ba1c3a833a6646ee
Asta Bankauske <astab@users.noreply.github.com>
2023-10-16 09:51:43 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://ovidius-astro-theme.netlify.app',
     integrations: [mdx(), sitemap(), tailwind()]
 });
