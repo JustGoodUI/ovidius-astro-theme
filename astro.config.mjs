bda0b7e397ea5a47745018bd60683d197359a5d1
Mikael Månsson <mikael.maansson@gmail.com>
2023-12-11 21:01:57 +0100

    fix disable base.css injections

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://ovidius-astro-theme.netlify.app',
-    integrations: [mdx(), sitemap(), tailwind()]
+    integrations: [mdx(), sitemap(), tailwind({
+        // Disable injecting a basic `base.css` import on every page.
+        config: { applyBaseStyles: false }
+    })]
 });
